987e4c7e6dd0c8b58fc9c9063c9e1cae547ba74c
Vinicius Brito <viniciusbritoo182363@gmail.com>
2026-06-09 21:42:57 -0400

    feat: add CidadeDto at CidadeController

--- src/main/java/edu/ifam/dad2026/ControlaDados/controller/CidadeController.java
+++ src/main/java/edu/ifam/dad2026/ControlaDados/controller/CidadeController.java
@@ -1,11 +1,19 @@
 package edu.ifam.dad2026.ControlaDados.controller;
 
 import edu.ifam.dad2026.ControlaDados.Repository.CidadeRepository;
+import edu.ifam.dad2026.ControlaDados.Repository.EstadoRepository;
+import edu.ifam.dad2026.ControlaDados.dto.CidadeInputDto;
+import edu.ifam.dad2026.ControlaDados.dto.CidadeOutputDto;
 import edu.ifam.dad2026.ControlaDados.model.Cidade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,43 +24,99 @@ public class CidadeController {
     @Autowired
     private CidadeRepository cidadeRepository;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Cidade> list() {
+    @Autowired
+    private EstadoRepository estadoRepository;
 
-        return cidadeRepository.findAll();
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CidadeOutputDto>> list() {
+
+        List<CidadeOutputDto> cidades = cidadeRepository.findAll()
+                .stream()
+                .map(CidadeOutputDto::new)
+                .toList();
+
+        return ResponseEntity.ok(cidades);
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Cidade getById(@PathVariable Long id) {
+    @GetMapping(value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CidadeOutputDto> getById(
+            @PathVariable Long id) {
 
-        Optional<Cidade> cidade = cidadeRepository.findById(id);
-        if (cidade.isPresent()) {
-            return cidade.get();
+        Optional<Cidade> cidadeOptional =
+                cidadeRepository.findById(id);
+
+        if (cidadeOptional.isPresent()) {
+            return ResponseEntity.ok(
+                    new CidadeOutputDto(cidadeOptional.get())
+            );
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        return null;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Cidade create(@RequestBody Cidade cidade) {
+    public ResponseEntity<CidadeOutputDto> create(
+            @RequestBody CidadeInputDto cidadeInputDto,
+            UriComponentsBuilder uriBuilder) {
 
-        return cidadeRepository.save(cidade);
+        cidadeInputDto.setEstadoRepository(estadoRepository);
+
+        Cidade cidade = cidadeInputDto.build();
+
+        Cidade cidadeSalva = cidadeRepository.save(cidade);
+
+        CidadeOutputDto cidadeOutputDto =
+                new CidadeOutputDto(cidadeSalva);
+
+        UriComponents uriComponents =
+                uriBuilder.path("/api/cidade/{id}")
+                        .buildAndExpand(cidadeOutputDto.getId());
+
+        URI uri = uriComponents.toUri();
+
+        return ResponseEntity.created(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(cidadeOutputDto);
     }
 
     @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<HttpStatus    > delete(@PathVariable Long id) {
 
-        cidadeRepository.deleteById(id);
+        Optional<Cidade> cidadeOptional = cidadeRepository.findById(id);
+
+        if (cidadeOptional.isPresent()) {
+            cidadeRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
     @PutMapping(value = "/{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Cidade update(@PathVariable Long id,
-                         @RequestBody Cidade cidade) {
+    public ResponseEntity<CidadeOutputDto> update(
+            @PathVariable Long id,
+            @RequestBody CidadeInputDto cidadeInputDto) {
 
-        cidade.setId(id);
+        Optional<Cidade> cidadeOptional = cidadeRepository.findById(id);
 
-        return cidadeRepository.save(cidade);
+        if (cidadeOptional.isPresent()) {
+
+            cidadeInputDto.setEstadoRepository(estadoRepository);
+
+            Cidade cidade = cidadeInputDto.build();
+            cidade.setId(id);
+
+            Cidade cidadeSalva = cidadeRepository.save(cidade);
+
+            return ResponseEntity.ok(
+                    new CidadeOutputDto(cidadeSalva)
+            );
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
